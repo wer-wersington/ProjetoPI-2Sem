ab6c0070526ed151cd0b26d27d9c0f39f6711a68
Eric Tavares <ets1603@gmail.com>
2024-09-12 17:07:14 -0300

    Criar Classe Usuario e Adicionar Pagina html do Login

---
+++ Projeto Catalogo Filmes/src/java/Controller/Usuario.java
@@ -0,0 +1,36 @@
+
+package Controller;
+
+public class Usuario {
+
+    int pkuser;
+    String email;
+    String  senha;
+    
+    
+    public int getPkuser() {
+        return pkuser;
+    }
+
+    public void setPkuser(int pkuser) {
+        this.pkuser = pkuser;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
+}
