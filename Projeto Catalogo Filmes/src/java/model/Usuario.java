2ebacba0735f42e42f2fc8c71ec36b4049891227
WALSSIMON DOS SANTOS SILVA SACRAMENTO <walssimon.sssacramen@senacsp.edu.br>
2024-10-03 21:52:41 -0300

    Atualizacao Registro

--- Projeto Catalogo Filmes/src/java/model/Usuario.java
+++ Projeto Catalogo Filmes/src/java/model/Usuario.java
@@ -2,21 +2,16 @@ package model;
 
 import Controller.ConectarDao;
 import Controller.IcrudDao;
+import java.sql.SQLException;
 
 public class Usuario extends ConectarDao implements IcrudDao  {
 
-    int pkuser;
-    String nome;
-    String email;
-    String senha;
 
-    public int getPkuser() {
-        return pkuser;
-    }
+    public String nome;
+    public String email;
+    public String senha;
 
-    public void setPkuser(int pkuser) {
-        this.pkuser = pkuser;
-    }
+
 
     public String getEmail() {
         return email;
@@ -49,7 +44,38 @@ public class Usuario extends ConectarDao implements IcrudDao  {
         this.nome = nome;
     }
     
-
+    
+    public void incluir(){
+    try { sql = "insert into TB_USUARIOS (NM_USUARIO, DS_EMAIL, DS_SENHA) "
+        + "values (?,?,?) ";
+    ps = con.prepareStatement(sql);
+    ps.setString(1, nome); // Configura Parametros
+    ps.setString(2, email); // Conf    igura Parametros
+    ps.setString(3, senha ); // Configura Parametros
+    ps.executeUpdate(); // executa comando SQL
+ this.statusSQL = null; // armazena null se deu tudo certo
+ } catch (SQLException ex) {
+ this.statusSQL = "Erro ao incluir usuario ! <br> " + ex.getMessage();
+ } }
+    /*
+    public boolean getLogin() {
+        if (email.equals("admin") && senha.equals("1234")) 
+        return true;
+ 
+    try {
+    sql = "select * from TB_USUARIOS where email = ? and senha = ?";
+    ps = con.prepareStatement(sql); // prepara SQL
+    ps.setString(1, email); // Configura Parametros
+    ps.setString(2, senha); // Configura Parametros
+    tab = ps.executeQuery(); // Executa comando SQL
+        if (tab.next()) return true;
+            this.statusSQL = null; // armazena null se deu tudo certo
+        } catch (SQLException ex) {
+        this.statusSQL = "Erro ao tentar buscar Usuário! " + ex.getMessage();
+    }
+ return false;
+ }
+*/
 @Override
 public boolean salvar() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
