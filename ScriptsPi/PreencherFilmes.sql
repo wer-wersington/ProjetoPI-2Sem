fcbb191e1c627a9ff941ea9d577b9de1c837ba97
walssimon <walssimon123@gmail.com>
2024-10-17 17:40:42 -0300

    Sql scripts

---
+++ ScriptsPi/PreencherFilmes.sql
@@ -0,0 +1,58 @@
+INSERT INTO TB_FILME(NM_FILME, DS_SINOPSE, HR_DURACAO, DT_LANCAMENTO, VL_AVALIACAO, NR_CLASSIFICACAO_INDICATIVA, IMG_CAPA, IMG_BANNER) 
+VALUES("Homem de Ferro", 
+    "Tony Stark é um industrial bilionário e inventor brilhante que realiza testes bélicos no exterior, mas é sequestrado por terroristas que o forçam a construir uma arma devastadora. Em vez disso, ele constrói uma armadura blindada e enfrenta seus sequestradores. Quando volta aos Estados Unidos, Stark aprimora a armadura e a utiliza para combater o crime."
+    ,020600, 20080430, null, 12, null, null)
+    ,("Homem de Ferro 2", 
+    "Em um mundo ciente da existência do Homem de Ferro, o inventor bilionário Tony Stark sofre pressão de todos os lados para compartilhar sua tecnologia com as forças armadas. Ele resiste para divulgar os segredos de sua inigualável armadura, com medo de que estas informações caiam nas mãos erradas. Com a bela Pepper Potts e o amigo 'Rhodey' Rhodes ao seu lado, Tony precisa forjar novas alianças e confrontar um inimigo poderoso."
+    ,020500, 20100430, null, 12, null, null)
+    ,("Homem de Ferro 3", 
+    "Depois de um inimigo reduzir o mundo de Tony Stark a destroços, o Homem de Ferro precisa aprender a confiar em seus instintos para proteger aqueles que ama, especialmente sua namorada, e lutar contra seu maior medo: o fracasso."
+    ,021000, 20130426, null, 12, null, null)
+    ,("Sonic: O Filme", 
+    "O porco-espinho Sonic é teletransportado para a Terra. Após causar uma pane elétrica em parte dos Estados Unidos, ele precisa escapar do maligno cientista Dr. Robotnik."
+    ,013900, 20200213, null, 10, null, null)
+    ,("Sonic 2 - O Filme", 
+    "O Dr. Robotnik retorna à procura de uma esmeralda mística que tem o poder de destruir civilizações. Para detê-lo, Sonic se une a seu antigo parceiro, Tails, e parte em uma jornada para encontrar a joia antes que ela caia em mãos erradas."
+    ,020200, 20220407, null, 10, null, null)
+    ,("Orgulho e Preconceito", 
+    "Elizabeth Bennet vive com sua mãe, pai e irmãs no campo, na Inglaterra. Por ser uma das filhas mais velhas, ela enfrenta uma crescente pressão de seus pais para se casar. Quando Elizabeth é apresentada ao belo e rico Darcy, faíscas voam. Embora haja uma química óbvia entre os dois, a natureza excessivamente reservada de Darcy ameaça a relação."
+    ,020700, 20060210, null, 12, null, null)
+    ,("A Baleia", 
+    "Homem obeso e solitário busca se reconectar com filha adolescente em uma última chance de redenção."
+    ,020700, 20230209, null, 16, null, null)
+    ,("Os Caras Malvados", 
+    "Depois de uma vida inteira de assaltos lendários, os notórios criminosos Sr. Lobo, Sr. Cobra, Sr. Piranha, Sr. Tubarão e Srta. Tarântula são finalmente capturados. Para evitar uma sentença de prisão, os bandidos animais devem realizar seu golpe mais desafiador até agora - tornando-se cidadãos-modelo. Sob a tutela de seu mentor, Professor Marmelada, a duvidosa gangue se propõe a enganar o mundo e mostrar que está se tornando boa."
+    ,014000, 20220317, null, 0, null, null)
+    ,("Space Jam: O Jogo do Seculo", 
+    "Um parque de diversões intergalático precisa de novas atrações! Um perigoso vilão espacial decide que Pernalonga e sua turma são os personagens mais divertidos do mundo e envia um grupo de 5 pequenos alienígenas, os Nerdlucks, para capturá-los."
+    ,012800, 19961225, null, 0, null, null)
+    ,("O Chamado", 
+    "A jornalista Rachel Keller decide investigar a morte de sua sobrinha e descobre que quatro adolescentes morreram misteriosamente sete dias depois de assistir a um vídeo com imagens assustadoras. Agora ela tenta solucionar o mistério e impedir que a profecia se realize, já que ela e seu filho também assistiram ao vídeo."
+    ,011500, 20030131, null, 14, null, null)
+    ,("Interestelar", 
+    "As reservas naturais da Terra estão chegando ao fim e um grupo de astronautas recebe a missão de verificar possíveis planetas para receberem a população mundial, possibilitando a continuação da espécie. Cooper é chamado para liderar o grupo e aceita a missão sabendo que pode nunca mais ver os filhos. Ao lado de Brand, Jenkins e Doyle, ele seguirá em busca de um novo lar."
+    ,024900, 20141106, null, 12, null, null)
+    ,("Click", 
+    "Um arquiteto casado e com filhos está cada vez mais frustrado por passar a maior parte de seu tempo trabalhando. Um dia, ele encontra um inventor excêntrico que lhe dá um controle remoto universal, com capacidade de acelerar o tempo. No início, ele usa o aparelho para acelerar qualquer momento tedioso, mas se dá conta de que está acelerando o tempo demais e deixando de viver preciosos momentos em família. Desesperado, ele procura o inventor para ajudá-lo a reverter o que fez."
+    ,014800, 20060811, null, 14, null, null)
+    ,("Laranja Mecânica", 
+    "O jovem Alex passa as noites com uma gangue de amigos briguentos. Depois que é preso, se submete a uma técnica de modificação de comportamento para poder ganhar sua liberdade."
+    ,021600, 19720426, null, 16, null, null)
+    ,("Truque de Mestre", 
+    "Um grupo de ilusionistas encanta o público com suas mágicas e também rouba bancos em outro continente, distribuindo a quantia para os próprios espectadores. O agente do FBI Dylan Hobbs está determinado a capturá-los e conta com a ajuda de Alma Vargas, uma detetive da Interpol, e também de Thaddeus Bradley, um veterano desmistificador de mágicos que insiste que os assaltos são realizados a partir de disfarces e jogos envolvendo vídeos."
+    ,011500, 20130705, null, 12, null, null)
+    ,("Batman Begins", 
+    "O jovem Bruce Wayne viaja para o Oriente e recebe treinamento em artes marciais do mestre Henri Ducard, um membro da misteriosa Liga das Sombras. Quando Ducard revela que a verdadeira proposta da Liga é a destruição completa de Gotham City, Wayne retorna à sua cidade natal com o intuito de livrá-la de criminosos e assassinos. Bruce assume a persona de Batman, o Cavaleiro das Trevas, e conta com a ajuda do mordomo Alfred e do expert Lucius Fox."
+    ,022000, 20050617, null, 14, null, null)
+    ,("Batman: O Cavaleiro das Trevas", 
+    "Batman tem conseguido manter a ordem em Gotham com a ajuda de Jim Gordon e Harvey Dent. No entanto, um jovem e anárquico criminoso, conhecido apenas como Coringa, pretende testar o Cavaleiro das Trevas e mergulhar a cidade em um verdadeiro caos."
+    ,023200, 20080718, null, 12, null, null)
+    ,("Batman: O Cavaleiro das Trevas Ressurge", 
+    "Após ser culpado pela morte de Harvey Dent e passar de herói a vilão, Batman desaparece. As coisas mudam com a chegada de uma ladra misteriosa, a Mulher-Gato, e Bane, um terrorista mascarado, que fazem Batman abandonar seu exílio forçado."
+    ,024200, 20120727, null, 12, null, null)
+    ,("O Feitiço de Áquila", 
+    "O bispo de Áquila descobre que a sua amada, Isabeau está apaixonada por um cavaleiro. Enciumado, o bispo resolve lançar uma maldição sobre o casal, na qual durante o dia Isabeau se transforma em um falcão e à noite o cavaleiro toma a forma de um lobo. Dessa maneira o casal não pode se entregar um ao outro. A única esperança do apaixonado casal é o amigo Phillipe, conhecido como Rato, o único que conseguiu escapar das muralhas de Áquila."
+    ,020100, 19850412, null, 12, null, null)
+    ,("Eu Sou a Lenda", 
+    "Robert Neville é um brilhante cientista e o único sobrevivente de uma epidemia que transformou os humanos em mutantes sedentos por sangue. Andando pela cidade de Nova York, ele procura por outros possíveis sobreviventes e tenta achar a cura da praga usando seu próprio sangue, que é imune."
+    ,014100, 20080118, null, 14, null,null)
